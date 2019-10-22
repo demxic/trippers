786436b8f9dc5f28ae4b022b0afb53671cff6845
demxic <demxic@gmail.com>
2019-10-22 16:59:03 -0500

    refactor : Modify airport_list_request_object

--- tests/request_objects/test_airport_list_request_objects.py
+++ tests/request_objects/test_airport_list_request_objects.py
@@ -1,3 +1,4 @@
+import pytest
 
 from tripper.request_objects import airport_list_request_object as req
 
@@ -5,33 +6,55 @@ from tripper.request_objects import airport_list_request_object as req
 def test_build_airport_list_request_object_without_parameters():
     request = req.AirportListRequestObject()
 
-    assert request.airport_codes is None
+    assert request.filters is None
     assert bool(request) is True
 
 
-def test_build_airport_list_request_object_with_empty_list():
-    request = req.AirportListRequestObject([])
+def test_build_airport_list_request_object_from_empty_dict():
+    request = req.AirportListRequestObject.from_dict({})
 
-    assert len(request.airport_codes) == 0
+    assert request.filters is None
     assert bool(request) is True
 
 
-def test_build_airport_list_request_object_with_rejected_codes():
-    airport_codes = ['MEX', 'ACAP', 'GDL', 'MT', 'FCO', 'M4D', '679', 'AM$', 'T@M', 'T#&']
-    request = req.AirportListRequestObject.from_airports_code_list(airport_codes= airport_codes)
+def test_build_airport_list_request_object_with_empty_filters():
+    request = req.AirportListRequestObject(filters={})
+    assert request.filters == {}
+    assert bool(request) is True
 
+
+def test_build_airport_list_request_object_from_dict_with_empty_filters():
+    request = req.AirportListRequestObject.from_dict({'filters': {}})
+    assert request.filters == {}
+    assert bool(request) is True
+
+
+def test_build_airport_list_request_object_from_dict_with_filters_wrong():
+    request = req.AirportListRequestObject.from_dict({'filters': {'a': 1}})
     assert request.has_errors()
-    assert request.errors[0]['parameter'] == 'airport_codes'
-    assert request.errors[0]['airport_codes'] == ['ACAP', 'MT', 'M4D', '679', 'AM$', 'T@M', 'T#&']
+    assert request.errors[0]['parameter'] == 'filters'
     assert bool(request) is False
 
 
-def test_build_airport_list_request_object_with_accepted_codes():
-    airport_codes = ['MEX', 'ACA', 'GDL', 'MTY', 'FCO']
-    request = req.AirportListRequestObject.from_airports_code_list(airport_codes=airport_codes)
+def test_build_airport_list_request_object_from_dict_with_invalid_filters():
+    request = req.AirportListRequestObject.from_dict({'filters': 5})
+    assert request.has_errors()
+    assert request.errors[0]['parameter'] == 'filters'
+    assert bool(request) is False
 
-    assert request.airport_codes == airport_codes
+
+@pytest.mark.parametrize('key', ['continent__eq', 'tz_city__eq', 'codes_list'])
+def test_build_airport_list_request_object_accepted_filters(key):
+    filters = {key: 'America'}
+    request = req.AirportListRequestObject.from_dict({'filters': filters})
+    assert request.filters == filters
     assert bool(request) is True
 
 
-
+@pytest.mark.parametrize('key', ['code__lt', 'code__gt'])
+def test_build_airport_list_request_object_rejected_filters(key):
+    filters = {key: 1}
+    request = req.AirportListRequestObject.from_dict({'filters': filters})
+    assert request.has_errors()
+    assert request.errors[0]['parameter'] == 'filters'
+    assert bool(request) is False
--- tripper/request_objects/airport_list_request_object.py
+++ tripper/request_objects/airport_list_request_object.py
@@ -1,10 +1,12 @@
-class InvalidRequestObject(object):
+import collections
 
-    def __init__(self):
-        self.errors = []
 
-    def add_error(self, parameter: str, message: str, airport_codes: list = None):
-        self.errors.append({'parameter': parameter, 'message': message, 'airport_codes': airport_codes})
+class InvalidRequestObject:
+    def __init__(self, errors: list):
+        self.errors = errors if errors else []
+
+    def add_error(self, parameter: str, message: str):
+        self.errors.append({'parameter': parameter, 'message': message})
 
     def has_errors(self):
         return len(self.errors) > 0
@@ -13,37 +15,39 @@ class InvalidRequestObject(object):
         return False
 
 
-class ValidRequestObject(object):
+class ValidRequestObject:
+
+    @classmethod
+    def from_dict(cls, adict):
+        raise NotImplementedError
 
     def __bool__(self):
         return True
 
-
 class AirportListRequestObject(ValidRequestObject):
     accepted_code_length = 3
+    accepted_filters = ['continent__eq', 'tz_city__eq', 'codes_list']
 
-    def __init__(self, airport_codes: list = None):
-        self.airport_codes = airport_codes
+    def __init__(self, filters=None):
+        self.filters = filters
 
     @classmethod
-    def from_airports_code_list(cls, airport_codes: list):
-        invalid_req = InvalidRequestObject()
+    def from_dict(cls, adict):
+        invalid_req = InvalidRequestObject([])
 
-        if airport_codes:
+        if 'filters' in adict:
+            if not isinstance(adict['filters'], collections.Mapping):
+                invalid_req.add_error('filters', 'Is not iterable')
+                return invalid_req
 
-            if not isinstance(airport_codes, list):
-                invalid_req.add_error('airports_code_list', 'Is not iterable')
-            elif len(airport_codes) == 0:
-                invalid_req.add_error('airports_code_list', 'List is empty!')
-            else:
-                wrong_airport_codes = []
-                for airport_code in airport_codes:
-                    if (len(airport_code) != cls.accepted_code_length) or not airport_code.isalpha():
-                        wrong_airport_codes.append(airport_code)
-                if len(wrong_airport_codes) > 0:
-                    invalid_req.add_error('airport_codes', 'Wrong airport codes!', wrong_airport_codes)
-
+            for key, value in adict['filters'].items():
+                if key not in cls.accepted_filters:
+                    invalid_req.add_error(
+                        'filters',
+                        'Key {} cannot be used'.format(key)
+                    )
         if invalid_req.has_errors():
             return invalid_req
+        return cls(filters=adict.get('filters', None))
 
-        return cls(airport_codes=airport_codes)
+
